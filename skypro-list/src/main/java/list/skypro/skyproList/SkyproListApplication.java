e892bad9d60e4e6b6b0773386ea25b5fecc5af9e
Moisuk-Dmitri <moisuk.d@gmail.com>
2024-08-05 15:43:18 +0300

    Initial commit

---
+++ skypro-list/src/main/java/list/skypro/skyproList/SkyproListApplication.java
@@ -0,0 +1,13 @@
+package list.skypro.skyproList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyproListApplication.class, args);
+    }
+
+}
